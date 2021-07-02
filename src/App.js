66abdccd1879a633f323a92e4aff87cc791307fd
rsharma4125 <rsharma4125@gmail.com>
2021-07-02 21:46:49 +0530

    @dashboard_@initalcommit_@moduleUser

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import CardGrey from './Components/Card/CardGrey';
+import Dashboardhome from './Dashboard/Dashboardhome/Dashboardhome';
 
 import './App.css';
 
 function App() {
   return (
-
+    <Dashboardhome />
   );
 }
 
