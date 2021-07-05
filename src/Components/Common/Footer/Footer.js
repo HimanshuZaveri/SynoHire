60ed403e2fb9bf3fed305cfd3f9bf18c90b31672
rsharma4125 <rsharma4125@gmail.com>
2021-07-05 15:44:30 +0530

    user@Footer Component Update@commit2

--- src/Components/Common/Footer/Footer.js
+++ src/Components/Common/Footer/Footer.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import footerimage from './footer.png'
+import React, { Fragment } from "react";
+import Footer_content from "./Footer_content";
+import ReactDOM from "react-dom";
+
 
 const Footer = () => {
-    return (
-        <div>
-            <img src = {footerimage} width="100%"></img>
-        </div>
-    )
-}
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Footer_content/>,
+        document.getElementById("footer_main")
+      )}
+    </Fragment>
+  );
+};
 
-export default Footer
+export default Footer;
---
+++ src/Components/Common/Footer/Footer_content.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import footerimage from '../../../Assets/Image/footer.png';
+
+function Footer_content() {
+    return (
+        <>
+            <img src = {footerimage} width="100%"></img>
+        </>
+    )
+}
+
+export default Footer_content
