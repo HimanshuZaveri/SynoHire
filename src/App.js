c47eb71a6706f54bd36d25ce87ff5d330d2a9eb9
im-rahulkgupta <rahulkumargupta85561@gmail.com>
2021-07-07 13:00:40 +0530

    user@help 03 completed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./Components/Common/NavBar/Navbar";
 import Home from "./Components/Pages/Home";
 import Dashboard from "./Components/Pages/Dashboard";
+import Help from "./Components/Pages/Help";
 
 function App() {
   return (
-    <Dashboard/>
+    <Help/>
   )
 }
 
